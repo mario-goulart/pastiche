e21424d93114e249df53858065cb1cbdb07db361
Mario Domenech Goulart <mario@parenteses.org>
2021-12-19 17:40:58 +0100

    example-app.scm: add captcha-api example

--- example-app.scm
+++ example-app.scm
@@ -1,8 +1,22 @@
 (include "pastiche.scm")
 (import pastiche)
-(import awful)
+(import awful srfi-1)
+
+(define (captcha-api)
+  ;; This is an example on how to use the captcha API.
+  (let ((web-eggs '("awful" "pastiche" "spiffy" "uri-common" "uri-generic")))
+    (lambda (message)
+      (case message
+        ((render) `(("Name of a CHICKEN egg for web development: "
+                     `(input (@ (type "text")
+                                (name "captcha-user-answer")
+                                (maxlength 30))))))
+        ((valid?) (member ($ 'captcha-user-answer) web-eggs string-ci=?))
+        ((consume! define-pages) (void))
+        (else (error 'captcha-api "Invalid message" message))))))
 
 (pastiche "/" "paste.db"
+          captcha-api: (captcha-api)
           awful-settings:
           (lambda (handler)
             (parameterize
@@ -11,4 +25,3 @@
                  (page-doctype "<!DOCTYPE html>")
                  (page-charset "UTF-8"))
               (handler))))
-
