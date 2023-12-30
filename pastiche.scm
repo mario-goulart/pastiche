4e150966fa5dbf5192ae0a9cf64166f16b4dec75
Mario Domenech Goulart <mario@parenteses.org>
2023-12-30 14:26:52 +0100

    Do not wrap pastes with literal
    The use of `literal` That causes text like `<foo>` not to be rendered
    in HTML, and also allows for some nasty injection issues.

--- pastiche.scm
+++ pastiche.scm
@@ -505,9 +505,9 @@
              (div (@ (class "paste"))
                   (pre (tt (@ (class "highlight scheme-language"))
                             ;; only colorize if the paste isn't too long
-                           (literal ,(if (< (string-length (fifth s)) 5000)
-                                         (html-colorize 'scheme (fifth s))
-                                         (fifth s))))))
+                           ,(if (< (string-length (fifth s)) 5000)
+                                (html-colorize 'scheme (fifth s))
+                                (fifth s)))))
              (div (@ (class "paste-footer"))
                   " [ "
                   (a (@ (href ,(make-pathname base-path
